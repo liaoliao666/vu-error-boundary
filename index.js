1f0585e1fcc58920f1638956722102104aa64c6b
liaoliao666 <1076988944@qq.com>
2021-02-03 10:15:17 +0800

    fix(index): entry file error

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./dist/vu-query.production.min.js')
+  module.exports = require('./dist/vu-error-boundary.production.min.js')
 } else {
-  module.exports = require('./dist/vu-query.development.js')
+  module.exports = require('./dist/vu-error-boundary.development.js')
 }
